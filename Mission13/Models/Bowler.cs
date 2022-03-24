2d88f6dd713cca29c6bd8438d0e163a335603c74
Benjamin Kloepfer <90466204+benjark@users.noreply.github.com>
2022-03-23 18:50:07 -0600

    1.0

---
+++ Mission13/Models/Bowler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mission13.Models
+{
+    public class Bowler
+    {
+        [Key]
+        [Required]
+        public string FirstName { get; set; }
+        public string MiddleInitial { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Zip { get; set; }
+        public string Phone { get; set; }
+
+    }
+}
---
+++ Mission13/Models/BowlingDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mission13.Models
+{
+    public class BowlingDbContext: DbContext
+    {
+        public BowlingDbContext(DbContextOptions<BowlingDbContext> options) : base (options)
+        {
+
+        }
+        public DbSet<Bowler> Bowlers { get; set; }
+    }
+}
