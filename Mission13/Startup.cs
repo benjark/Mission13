cba71eb250b8479c11358867e0c1683f148e314b
Benjamin Kloepfer <90466204+benjark@users.noreply.github.com>
2022-03-23 21:33:39 -0600

    1.0.1

--- Mission13/Startup.cs
+++ Mission13/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Mission13.Models;
 
 namespace Mission13
 {
@@ -24,6 +26,12 @@ namespace Mission13
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<BowlingDbContext>(options =>
+           {
+               options.UseMySql(Configuration["ConnectionStrings:BowlingDbConnection"]);
+
+           });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
